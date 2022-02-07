e4a96c12238f80e4b3a886007be3a3deae9d3ce2
Yasnair Angulo <yasnair@loka.com>
2022-02-07 14:18:40 +0100

    Update url

--- music_app/urls.py
+++ music_app/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import playlist_detail
 from . import views
 
 #URLConf
 urlpatterns = [
     #path('users/', UserView.as_view()), 
     path('users/', views.user_list),
-    path('users/<id>/', views.user_detail),
-    path('playlist/<int:pk>/', views.playlist_detail, name='playlist-list')
-
-    
+    path('users/<id>/', views.user_detail), 
 ]
